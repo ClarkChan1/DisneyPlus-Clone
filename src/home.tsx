483031d8834e6cd705dda71ace29b20d778908eb
ClarkChan1 <>
2023-10-20 13:42:41 -0700

    chore: added disney font

--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,32 @@
 import "./home.css";
 function Home() {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <div className="row-container">
+        <p className="section-title">Animated Movies</p>
+        <div className="content-container">
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+        </div>
+      </div>
+      <div className="row-container">
+        <p className="section-title">Walt Disney Animation Studios</p>
+        <div className="content-container">
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+        </div>
+      </div>
+      <div className="row-container">
+        <p className="section-title">Star Wars</p>
+        <div className="content-container">
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+          <img src="" alt="" className="content-img" />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
