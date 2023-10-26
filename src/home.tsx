b98d8cc8c2f50bbfa64cd83a3167d48599af7bc5
ClarkChan1 <>
2023-10-25 21:45:34 -0700

    chore: added navigation buttons for image slider

--- src/home.tsx
+++ src/home.tsx
@@ -1,14 +1,11 @@
 import "./home.css";
+import ImageSlider from "./image-slider";
 function Home() {
   return (
     <div className="home-container">
       <div className="row-container">
         <p className="section-title">Animated Movies</p>
-        <div className="content-container">
-          <img src="" alt="" className="content-img" />
-          <img src="" alt="" className="content-img" />
-          <img src="" alt="" className="content-img" />
-        </div>
+        <ImageSlider></ImageSlider>
       </div>
       <div className="row-container">
         <p className="section-title">Walt Disney Animation Studios</p>
