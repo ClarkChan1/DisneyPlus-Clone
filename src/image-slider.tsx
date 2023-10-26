f85e68518e57cf9003378ad8336ccf2a4f52571f
ClarkChan1 <>
2023-10-25 22:32:52 -0700

    chore: got basic image slider scroll to work on button click

--- src/image-slider.tsx
+++ src/image-slider.tsx
@@ -1,8 +1,18 @@
+import { useRef } from "react";
 import "./image-slider.css";
 function ImageSlider() {
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
   return (
     <div className="slider-container">
-      <button className="nav-button">
+      <button
+        className="nav-button"
+        onClick={() => {
+          const container = sliderRef.current;
+          const imageWidth = imageRef.current?.offsetWidth || 0;
+          container!.scrollLeft -= 2 * imageWidth;
+        }}
+      >
         <svg
           aria-label="arrowLeft"
           color="white"
@@ -17,11 +27,12 @@ function ImageSlider() {
           <path d="M22.324 28.008c.537.577.355 1.433-.326 1.809a1.44 1.44 0 0 1-.72.183c-.398 0-.786-.151-1.048-.438L10.06 18.588a1.126 1.126 0 0 1 0-1.555L20.233 6.09c.438-.468 1.198-.564 1.767-.25.681.377.863 1.23.325 1.808l-9.446 10.164 9.446 10.196zM11.112 17.615a.31.31 0 0 1 0 .391l.182-.195-.182-.196zM21.308 7.094c-.01-.006-.053 0-.029-.027a.07.07 0 0 0 .029.027zm-.025 21.499a.95.95 0 0 1-.006-.008l.006.008z"></path>
         </svg>
       </button>
-      <div className="content-container">
+      <div className="content-container" ref={sliderRef}>
         <img
           src="../images/clone-wars-pic.jpg"
           alt=""
           className="content-img"
+          ref={imageRef}
         />
         <img
           src="../images/clone-wars-pic.jpg"
@@ -49,7 +60,14 @@ function ImageSlider() {
           className="content-img"
         />
       </div>
-      <button className="nav-button">
+      <button
+        className="nav-button"
+        onClick={() => {
+          const container = sliderRef.current;
+          const imageWidth = imageRef.current?.offsetWidth || 0;
+          container!.scrollLeft += 2 * imageWidth;
+        }}
+      >
         <svg
           aria-hidden="true"
           aria-label="arrowRight"
