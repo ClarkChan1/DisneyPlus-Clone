bc8b5e577eb121a1a274bb17b12ab0d188ea166d
ClarkChan1 <>
2023-11-03 20:48:38 -0700

    chore: add clear button functionality for searchbar

--- src/search.tsx
+++ src/search.tsx
@@ -5,12 +5,6 @@ import SearchGrid from "./searchgrid";
 function Search() {
   const [movies, updateMovies] = useState<string[]>([]);
   const [tv, updateTV] = useState<string[]>([]);
-  // function handleUpdateMovies(content: string[]) {
-  //   updateMovies(content);
-  // }
-  // function handleUpdateTV(content: string[]) {
-  //   updateTV(content);
-  // }
   return (
     <div className="search-section">
       <Searchbar updateMovies={updateMovies} updateTV={updateTV} />
--- src/searchbar.tsx
+++ src/searchbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, Dispatch, SetStateAction } from "react";
+import { useEffect, useState, Dispatch, SetStateAction, useRef } from "react";
 import "./searchbar.css";
 import getPosters from "../utils/getPosters";
 interface Props {
@@ -8,6 +8,7 @@ interface Props {
 
 function Searchbar(props: Props) {
   const [searchText, updateSearchText] = useState("");
+  const textBoxRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     async function getData() {
       // get movies
@@ -28,6 +29,7 @@ function Searchbar(props: Props) {
   return (
     <div className="searchbar-container">
       <input
+        ref={textBoxRef}
         className="searchbar"
         type="text"
         placeholder="Search by title, character, or genre"
@@ -38,22 +40,30 @@ function Searchbar(props: Props) {
           updateSearchText(e.target.value);
         }}
       ></input>
-      <div className="clear-button">
-        <svg
-          aria-hidden="true"
-          aria-label="close"
-          color="#f9f9f9"
-          role="img"
-          transform=""
-          version="1.1"
-          viewBox="0 0 36 36"
-          xmlns="http://www.w3.org/2000/svg"
-          className="x-icon"
+      {searchText != "" ? (
+        <div
+          className="clear-button"
+          onClick={() => {
+            textBoxRef.current!.value = "";
+            updateSearchText("");
+          }}
         >
-          <title></title>
-          <path d="M17.9 16.239l8.833-9.085c.255-.266.629-.404 1.01-.404.248 0 .491.056.706.175.641.338.817 1.121.301 1.649l-9.081 9.342 9.08 9.312c.517.53.343 1.311-.313 1.657-.546.287-1.276.2-1.7-.231l-8.835-9.062-8.838 9.064c-.421.428-1.152.516-1.699.229-.655-.346-.829-1.127-.312-1.657l9.08-9.312-9.08-9.341c-.516-.529-.34-1.312.313-1.657.202-.112.445-.168.693-.168.382 0 .755.138 1.007.4l8.836 9.089z"></path>
-        </svg>
-      </div>
+          <svg
+            aria-hidden="true"
+            aria-label="close"
+            color="#f9f9f9"
+            role="img"
+            transform=""
+            version="1.1"
+            viewBox="0 0 36 36"
+            xmlns="http://www.w3.org/2000/svg"
+            className="x-icon"
+          >
+            <title></title>
+            <path d="M17.9 16.239l8.833-9.085c.255-.266.629-.404 1.01-.404.248 0 .491.056.706.175.641.338.817 1.121.301 1.649l-9.081 9.342 9.08 9.312c.517.53.343 1.311-.313 1.657-.546.287-1.276.2-1.7-.231l-8.835-9.062-8.838 9.064c-.421.428-1.152.516-1.699.229-.655-.346-.829-1.127-.312-1.657l9.08-9.312-9.08-9.341c-.516-.529-.34-1.312.313-1.657.202-.112.445-.168.693-.168.382 0 .755.138 1.007.4l8.836 9.089z"></path>
+          </svg>
+        </div>
+      ) : null}
     </div>
   );
 }
